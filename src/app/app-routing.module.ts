01da4174e1ccd7a57dfebae9c9c5a6cec956c735
somia Salman <somia.salman93@gmail.com>
2024-02-10 21:29:08 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent,title:'HOME'},
+  {path:'about',component:AboutComponent,title:'ABOUT'},
+  {path:'portfolio',component:PortfolioComponent,title:'PORTFOLIO'},
+  {path:'contact',component:ContactComponent,title:'CONTACT'},
+  {path:'',component:PortfolioComponent},
+  {path:'**',component:NotFoundComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration:'top'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
